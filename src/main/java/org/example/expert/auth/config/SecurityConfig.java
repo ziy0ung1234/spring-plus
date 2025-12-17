8db849037ac8fce0eef14292bec222b90d238145
ziy0ung1234 <dnjsejrjfsm1@gmail.com>
2025-12-17 17:26:18 +0900

    [Level2] refactor: security+jwt로 수정

---
+++ src/main/java/org/example/expert/auth/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package org.example.expert.auth.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.example.expert.auth.filter.JwtAuthenticationFilter;
+import org.example.expert.config.JwtFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)  // .csrf().disable() 방식은 더 이상 사용 안함.
+                .httpBasic(AbstractHttpConfigurer::disable) // BasicAuthenticationFilter 비활성화
+                .formLogin(AbstractHttpConfigurer::disable) // UsernamePasswordAuthenticationFilter, DefaultLoginPageGeneratingFilter 비활성화
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class) // JWT 인증 필터는 항상 UsernamePasswordAuthenticationFilter 앞에 위치해야 한다.
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST,"/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
+}
---
+++ src/main/java/org/example/expert/auth/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,78 @@
+package org.example.expert.auth.filter;
+
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.expert.config.JwtUtil;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ *  PrincipalDetailService - username/password -> DB에서 사용자 정보를 불러오기 위한 책임
+ *  굳이 UserDetail 구현체가 필요 없는 이유
+ *  - JWT Filter에서 직접 검증
+ *  - claim에 userid 있음 -> Controller에서 userID만 알면 됨
+ *  -
+ * */
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String uri = request.getRequestURI();
+        return request.getRequestURI().startsWith("/auth/");
+    }
+
+    //Authentication을 만들고 SecurityContext에 넣어주는 메서드
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken == null) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "JWT 토큰이 필요합니다.");
+            return;
+        }
+
+        try {
+            String token = jwtUtil.substringToken(bearerToken);
+            Claims claims = jwtUtil.extractClaims(token);
+
+            Long userId = Long.parseLong(claims.getSubject()); // JWT의 subject를 사용자 식별자
+            String role = claims.get("userRole", String.class);
+
+            List<GrantedAuthority> authorities =
+                    List.of(new SimpleGrantedAuthority("ROLE_" + role));
+
+            Authentication authentication =
+                    new UsernamePasswordAuthenticationToken(
+                            userId,        // principal
+                            null,
+                            authorities
+                    );
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            filterChain.doFilter(request, response);
+
+        } catch (Exception e) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "유효하지 않은 JWT 토큰입니다.");
+        }
+    }
+}
